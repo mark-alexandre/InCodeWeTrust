5b35bf57f62ce91dc76ebec99092e47a054eb365
Vincent <v.fritsch1981@gmail.com>
2020-06-24 22:47:41 +0200

    API install, now fixed view and continue from

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,18 +2,44 @@
 
 namespace App\Controller;
 
+use Psr\SimpleCache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenFoodFacts;
 
 class ApiController extends AbstractController
 {
     /**
-     * @Route("/api", name="api")
+     * @Route("/api/{product}/{txsalt}", name="api")
+     * @param float $txsalt
+     * @return Response
+     * @throws OpenFoodFacts\Exception\BadRequestException
+     * @throws InvalidArgumentException
      */
-    public function index()
+    public function index(string $product, float $txsalt)
     {
+        $api = new OpenFoodFacts\Api('food','fr');
+
+        $products = $api->search($product);
+
+        $pro =[];
+        foreach($products as $k)
+        {
+            if (isset($k->getData()['nutriments']['salt']))
+            {
+                if ($k->getData()['nutriments']['salt']< $txsalt)
+                {
+                    $pro[] = $k;
+                }
+            }
+
+        }
+
         return $this->render('api/index.html.twig', [
             'controller_name' => 'ApiController',
+            'product' => $products,
+            'pro' => $pro
         ]);
     }
 }
